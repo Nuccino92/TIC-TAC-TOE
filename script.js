bd06ecda87f2cdd10ce051f9c8e424b527d13f35
Anthony Nucci <anthnucci@hotmail.com>
2021-08-24 03:10:05 -0400

    finished the set up hud styling, also added js logic for switching between computer and second player in the hud.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const openComputerButton = document.querySelectorAll('[data-open-computer]')
+const openPlayerButton =  document.querySelectorAll('[data-open-player]')
+
+openComputerButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const computerUi = document.getElementById('computerUi');
+        openComputer(computerUi);
+    })
+});
+
+openPlayerButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const playerUi = document.getElementById('playerUi');
+        openPlayer(playerUi);
+    })
+});
+
+function openComputer(computerUi) {
+    computerUi.classList.add('active');
+    playerUi.classList.add('active');
+};
+
+function openPlayer(playerUi) {
+    playerUi.classList.remove('active');
+    computerUi.classList.remove('active');
+};
+
+// All code above selects the computer/player two UI
+// ---------------------------------------------------------------------
 
 const Gameboard = (function() {
     let board = [];
