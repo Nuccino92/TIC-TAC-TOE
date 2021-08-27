68b42c66d7f513210949b86cb814e5b339c778dc
Anthony Nucci <anthnucci@hotmail.com>
2021-08-27 02:40:38 -0400

    Added update board array function, also added click events and fucntions to place x's and o's on the board, currently working on the reset function along with the buttons to have player 1 select which mark he would like to start off with(x or o)

--- script.js
+++ script.js
@@ -1,60 +1,135 @@
-const openComputerButton = document.querySelectorAll('[data-open-computer]')
-const openPlayerButton =  document.querySelectorAll('[data-open-player]')
+const DisplayChange = (() => {
+    
+    const openComputerButton = document.querySelectorAll('[data-open-computer]')
+    const openPlayerButton =  document.querySelectorAll('[data-open-player]')
 
-openComputerButton.forEach(button => {
-    button.addEventListener('click', () => {
-        const computerUi = document.getElementById('computerUi');
-        openComputer(computerUi);
+    openComputerButton.forEach(button => {
+        button.addEventListener('click', () => {
+            const computerUi = document.getElementById('computerUi');
+            openComputer(computerUi);
+        })
+    });
+
+    openPlayerButton.forEach(button => {
+        button.addEventListener('click', () => {
+            const playerUi = document.getElementById('playerUi');
+            openPlayer(playerUi);
+        })
+    });
+
+    function openComputer(computerUi) {
+        computerUi.classList.add('active');
+        playerUi.classList.add('active');
+    };
+
+    function openPlayer(playerUi) {
+        playerUi.classList.remove('active');
+        computerUi.classList.remove('active');
+    };
+
+    const playButton = document.getElementById('play-button');
+    const returnButton = document.getElementById('return');
+    const setupUi = document.getElementById('setup-ui');
+    const gameUi = document.getElementById('game-ui');
+
+    playButton.addEventListener('click', () => {
+        openGame()
+    });
+
+    returnButton.addEventListener('click', () => {
+        setupReturn()
+        Gameboard.resetBoard()
     })
-});
 
-openPlayerButton.forEach(button => {
-    button.addEventListener('click', () => {
-        const playerUi = document.getElementById('playerUi');
-        openPlayer(playerUi);
-    })
-});
+    function openGame() {
+        setupUi.classList.add('active');
+        gameUi.classList.add('active');
+    };
 
-function openComputer(computerUi) {
-    computerUi.classList.add('active');
-    playerUi.classList.add('active');
-};
+    function setupReturn() {
+        setupUi.classList.remove('active');
+        gameUi.classList.remove('active');
+    }
+})();
 
-function openPlayer(playerUi) {
-    playerUi.classList.remove('active');
-    computerUi.classList.remove('active');
-};
+const Gameboard = (() => {
+    let cells = document.querySelectorAll('.cell')
+    let board = ['', '', '', '', '', '', '', '', ''];
+    
+    const changeBoard = (num, mark) => {
+        board[num] = mark;
+    }
+    
+    const renderCells = () => {
+        for(i = 0; i < board.length; i++) {
+            cells[i].textContent = board[i];     
+        }
+    }
+    
+    function resetBoard() {
+        // for(i = 0; i < board.length; i++) {
+        //     board[i] = '';
+        //     cells[i].textContent = '';
+        //     Players.oTurn = false;
+        // }
+    }
+    
+    return {board, cells, renderCells, changeBoard, resetBoard};
+    
+})();
 
-// All code above selects the computer/player two UI
-// ---------------------------------------------------------------------
+const Players = (() => {
+    
+    const xMark = 'x';
+    const oMark = 'o';
+    let oTurn;
+    
+    const xButton = document.querySelector('.x-button-one')
+    const oButton = document.querySelector('.o-button-one')
 
-const playButton = document.getElementById('play-button');
-const returnButton = document.getElementById('return');
-const setupUi = document.getElementById('setup-ui');
-const gameUi = document.getElementById('game-ui');
+    xButton.addEventListener('click', setTurnX);
+        
+    oButton.addEventListener('click', setTurnO);
 
-playButton.addEventListener('click', () => {
-    openGame()
-});
+    function setTurnX() {
+        console.log('chicken')
+    }
 
-returnButton.addEventListener('click', () => {
-    setupReturn()
-})
+    function setTurnO() {
+        (oTurn = !oTurn) 
+    }
 
-function openGame() {
-    setupUi.classList.add('active');
-    gameUi.classList.add('active');
-};
+    Gameboard.cells.forEach(cell  => {
+        if(Gameboard.resetBoard) {
+            cell.addEventListener('click', cellClicked, {once: false})
+        }else
+            cell.addEventListener('click', cellClicked, {once: true})
+    }) 
+    
+    function cellClicked(e) { 
+        let cell = this.id;
+        let currentMark = oTurn ? oMark : xMark;
+        placeSelection(cell, currentMark);
+        switchTurn();
+    }
 
-function setupReturn() {
-    setupUi.classList.remove('active');
-    gameUi.classList.remove('active');
-}
-
-
-
-const Gameboard = (function() {
-    let board = [];
-
+    function placeSelection(cell, currentMark) {
+        Gameboard.changeBoard(cell, currentMark)
+        Gameboard.renderCells()
+    }
+   
+    function switchTurn() {
+        (oTurn = !oTurn)  
+    }
+      
+     return {oTurn}
 
 })();
+
+
+
+
+
+
+
+
